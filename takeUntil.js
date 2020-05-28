2d258ca8dfaa0dc714d622c17166b383c89a0da8
Jaycob Duffel <jbdcoding@gmail.com>
2020-05-28 21:27:20 +0000

    debugged takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -14,10 +14,10 @@ const takeUntil = function(arr, cb) {
 };
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
+const results1 = takeUntil(data1, x => x < -3);
 console.log(results1);
 
-console.log('---');
+console.log('\n --- \n');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
