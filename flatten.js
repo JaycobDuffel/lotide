9326d5337ad16d33052802185a5d66f6de331a00
Jaycob Duffel <jbdcoding@gmail.com>
2020-06-01 00:17:13 +0000

    initial flatten commit

---
+++ flatten.js
@@ -0,0 +1,46 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+
+}
+const assertArrays = (arr1, arr2) => {
+  // let result = true;
+  // if (arr1.length !== arr2.length) {
+  //   result = false;
+  // }
+  // for (let i = 0; i < arr1.length; i++) {
+  //   if (arr1[i] !== arr2[i]) {
+  //     result = false;
+  //   }
+  // }
+
+  
+  
+
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ ${arr1} matches ${arr2} ✅✅✅`);
+  } else {
+    console.log(`🛑🛑🛑 ${arr1} does not match ${arr2} 🛑🛑🛑`);
+  }
+};
+const flatten = (array) => {
+  let output = [];
+  array.forEach(item => {
+    if (Array.isArray(item)) {
+      output = output.concat(flatten(item))
+    } else {
+    output.push(item);
+  }
+});
+return output;
+}
+assertArrays(flatten([1, 2, [3, 4], 5, [23, 67, 19], [6]]), [1, 2, 3, 4, 5, 23, 67, 19, 6])
+
+
